45f7c6722aaeccd91601647529ce76204f330e57
purplemoonboi <purplemoonone@gmail.com>
2021-07-04 10:36:53 +0100

    Converted Foundation to a static library

--- Premake5.lua
+++ Premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -19,9 +20,7 @@ project "GLFW"
 	}
 
 	filter "system:windows"
-	 buildoptions {"-std=c11", "-lgdi32"}
 	 systemversion "latest"
-	 staticruntime "On"
 
 	files 
 	{
@@ -42,9 +41,12 @@ project "GLFW"
 		"_CRT_SECURE_NO_WARNINGS"
 	}
 
-	filter
-	{
-		"system:windows", 
-		"configurations:Release"
-	}
-	buildoptions "/MT"
+	filter "configurations:Debug"
+			defines	"FD_DEBUG"
+			runtime "Debug"
+			symbols "on"
+
+			filter "configurations:Release"
+			defines	"FD_RELEASE"
+			runtime  "Release"
+			optimize "on"
